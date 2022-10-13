10a30b0fc0ddb1d05351796f165ef4cee979af46
RaviYadav3003 <114741865+RaviYadav3003@users.noreply.github.com>
2022-10-13 20:40:17 +0530

    fix : fix error in API

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ var btnTranslate = document.querySelector(".translateButton");
 
 var translatedTextArea = document.querySelector(".translatedTextArea");
 
+var errormsgvalue = document.querySelector(".errormsg")
 var Url = "https://api.funtranslations.com/translate/valyrian";
 
 function getTranslationURL(text) {
   return Url + "?" + "text=" + text;
 }
 
+function errorHandler(error){
+  alert("something wrong with server! try again after some time")
+}
+
 function clickHandler() {
   var userInput = textAreaToTranslate.value;
   console.log(userInput);
+  
   fetch(getTranslationURL(userInput))
     .then((response) => response.json())
     .then((json) => {
@@ -20,7 +26,10 @@ function clickHandler() {
       translatedTextArea.innerText = translatedText;
       console.log(translatedText);
       console.log(translatedTextArea.value);
-    });
+    }).catch(errorHandler)
+   
 }
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
